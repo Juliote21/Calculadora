71b893e7761d148cc71a0f919ffba359b0abf803
Julio Alves <juli_alves_19@hotmail.com>
2019-10-22 17:45:22 -0300

    Carga inicial de calculadora

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Usuario
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewJFrame JFrame = new NewJFrame();
+        JFrame.setVisible(true);
+        
+    }
+    
+}
